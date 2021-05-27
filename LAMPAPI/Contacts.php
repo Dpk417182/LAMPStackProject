2ba2182801a4bc2aec3536cdb4fa54b5e4018c84
Jordan Stebbins <46496370+NyctophileSS@users.noreply.github.com>
2021-05-26 23:05:10 -0400

    Make Contacts php by accident

---
+++ LAMPAPI/Contacts.php
@@ -0,0 +1,60 @@
+
+<?php
+	$inData = getRequestInfo();
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+	$phoneNumber = "";
+	
+	$conn = new mysqli("localhost", "APIUser", "ProjectOne", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE ID = ?");
+		$stmt->bind_param("i", $inData["id"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if($result->num_rows > 0)
+		{
+			
+			$rows = array();
+						
+            $inc = 0;
+            while ($row = $result->fetch_assoc()) {
+                $jsonArrayObject = (array('ID' => $row["ID"], 'FirstName' => $row["FirstName"], 'LastName' => $row["LastName"], 'PhoneNumber' => $row["PhoneNumber"]));
+                $rows[$inc] = $jsonArrayObject;
+                $inc++;
+            }
+			sendResultInfoAsJson($rows);
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		
+		echo json_encode($obj);
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{  "id":0,"firstName":"","lastName":"","phoneNumber":"","error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+?>
